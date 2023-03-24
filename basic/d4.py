6142d48d12d3b848a73562a78ceb03f098210f86
Hong Mintaek <56999067+Marker0724@users.noreply.github.com>
2023-03-24 15:01:04 +0900

    feat: 로그인 기능 함수화 웹 프로그램에서 로그인 기능 함수를 호출 연동 완료

--- basic/d4.py
+++ basic/d4.py
@@ -3,42 +3,47 @@
 '''
 import pymysql as my
 
-connection = None
+def login_db() :
+    connection = None
 
-try:
-    connection = my.connect(host = 'localhost',
-                            port = 3306,
-                            user = 'root',
-                            password = '12341234',
-                            database = 'ml_db',
-                            cursorclass = my.cursors.DictCursor
-                            )
+    try:
+        connection = my.connect(host = 'localhost',
+                                port = 3306,
+                                user = 'root',
+                                password = '12341234',
+                                database = 'ml_db',
+                                cursorclass = my.cursors.DictCursor
+                                )
 
-    with connection.cursor() as cursor :
-        # 파라미터는 %s 표시로 순서대로 세팅된다. '값' -> ''는 자동으로 세팅된다.
-        sql = '''
-            SELECT 
-                uid, `name`, regdate
-            FROM
-                users
-            WHERE
-                uid=%s
-            AND
-                upw=%s;
-        '''
-        # execute() 함수의 2번 인자가 파라미터 전달하는 자리, 튜플로 표현
-        cursor.execute(sql, ('guest', '1234'))
-        row = cursor.fetchone()
-        print(row['name'])
-        pass 
+        with connection.cursor() as cursor :
+            # 파라미터는 %s 표시로 순서대로 세팅된다. '값' -> ''는 자동으로 세팅된다.
+            sql = '''
+                SELECT 
+                    uid, `name`, regdate
+                FROM
+                    users
+                WHERE
+                    uid=%s
+                AND
+                    upw=%s;
+            '''
+            # execute() 함수의 2번 인자가 파라미터 전달하는 자리, 튜플로 표현
+            cursor.execute(sql, ('guest', '1234'))
+            row = cursor.fetchone()
+            print(row['name'])
+            pass 
 
-except Exception as e:
-    print('접속 오류', e)
+    except Exception as e:
+        print('접속 오류', e)
 
-else:
-    print('접속 시 문제 없었다.')
+    else:
+        print('접속 시 문제 없었다.')
 
-finally:
-    if connection:
-        connection.close()
+    finally:
+        if connection:
+            connection.close()
 
+if __name__ == '__main__':
+    # d4 개발자의 테스트 코드
+    # f5 개발자가 사용할 때는 작동 안함
+    login_db()
