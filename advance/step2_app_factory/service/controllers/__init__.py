a063b77bad056fe23f3905b95869a9e24adac806
Hong Mintaek <56999067+Marker0724@users.noreply.github.com>
2023-03-27 14:35:42 +0900

    feat: 부트스트랩 적용 기준 가이드 설정 완료

--- advance/step2_app_factory/service/controllers/__init__.py
+++ advance/step2_app_factory/service/controllers/__init__.py
@@ -4,13 +4,13 @@ from flask import Blueprint
 bp_main = Blueprint('main_bp', # 별칭, 해당 블루프린트 밑에서 정의된 라우트 및 함수를 url_for('main_bp.home')로 지칭할 때 사용
                     __name__, # 고정
                     url_prefix='/main', # 모든 URL 앞에 /main이 추가된다.
-                    template_folder='../templates', # html이 위치한 경로
+                    template_folder='../templates/main', # html이 위치한 경로
                     static_folder='../static' # 정적데이터의 위치 경로
                     )
 
 bp_auth = Blueprint('auth_bp', 
                     __name__, 
                     url_prefix='/auth',
-                    template_folder='../templates',
+                    template_folder='../templates/auth',
                     static_folder='../static'
                     )
