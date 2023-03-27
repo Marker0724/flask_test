bc5f5194d3cd58a035eecf55423892f6efd5a536
Hong Mintaek <56999067+Marker0724@users.noreply.github.com>
2023-03-27 16:07:11 +0900

    feat: html 폼생성 및 wtf 활용

--- advance/step2_app_factory/service/__init__.py
+++ advance/step2_app_factory/service/__init__.py
@@ -9,10 +9,26 @@ from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
-
+    # 환경 변수 초기화
+    init_environment(app)
+    # 블루 프린트 초기화
     init_blueprint(app)
     
     return app
+
+def init_environment(app):
+    # 특정 파일(cfg, ...) 등을 읽어서 처리 가능
+    app.config.from_pyfile('resource/config.cfg', silent = True)
+    # py를 모듈가져오기 해서 (깩체)를 세팅해서 처리
+    import service.config as config
+    app.config.from_object(config)
+    # 환경변수(OS 레벨, 플라스크 레벨, 사용자 정의 레벨) 모두 출력
+    print('\n' + '-'*20)
+    # 개별 환경 변수값 추출
+    print(app.config['SECRET_KEY'], app.config.get('SECRET_KEY'))
+#    for k, v in app.config.items():
+#        print(k, v)
+    print('\n' + '-'*20)
 
 def init_blueprint(app):
     # app에 블루프린트 객체를 등록한다.
---
+++ advance/step2_app_factory/service/config/__init__.py
@@ -0,0 +1,3 @@
+SECRET_KEY = 'sdlbakjfbkjadnalkbadv' # 서비스 시 추론이 불가한 해시값 추천
+
+# 클래스를 정의해서 내부에 멤버로 표현해도 된다.
