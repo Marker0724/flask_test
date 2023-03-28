dd4ab46af32b823b3948f00574982d01ab7f44e2
Hong Mintaek <56999067+Marker0724@users.noreply.github.com>
2023-03-28 17:39:00 +0900

    feat: jwt 토큰 발급 및 해싱

--- advance/step2_app_factory/service/controllers/auth_controller.py
+++ advance/step2_app_factory/service/controllers/auth_controller.py
@@ -4,14 +4,17 @@
     - 비즈니스 로직 : 사용자가 요청하는 주 내용을 처리하는 곳
 '''
 
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, jsonify
+from flask import current_app # Flask 객체 획득
 from service.controllers import bp_auth as auth
+# 시간 정보를 획득, 시간차를 계산하는 함수
+from datetime import datetime, timedelta
+import jwt
 
 
 @auth.route('/')
 def home():
     # url_for(별칭.함수명) => url이 리턴된다.
-    print(url_for('auth_bp.login'))
     return '인증 홈'
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -20,7 +23,24 @@ def login():
         return render_template('login.html')
     else:
         # jwt 관련 체크 -> 정상(200), 오류(401)
-        return 'jwt 체크 완료'
+        # 1. uid, upw 획득
+        uid = request.form.get('uid')
+        upw = request.form.get('upw')
+        # 2. uid, upw로 회원이 존재하는지 체크 -> (원래 디비, 임시로 값 비교)
+        if uid=='guest' and upw=='1234':
+            # 3. 회원이라면 토큰 생성 (규격, 만료시간, 암호 알고리즘 지정, ...)
+            payload = { # 저장할 정보는 알아서 구성 (고객 정보가 기반)
+                'id' : uid,
+                # 만료 시간 (원하는대로 결정)
+                'exp': datetime.utcnow() + timedelta(seconds=60*60*24)
+            }
+            # 토큰 발급 => 시크릿키, 해시 알고리즘('HS256'), 데이터(payload)
+            SECRET_KEY = current_app.config['SECRET_KEY'] # 환경변수 값 획득
+            # 발급
+            token = jwt.encode(payload, SECRET_KEY, algorithm = 'HS256')
+            
+            # 4. 응답 전문 구성 -> 응답
+            return jsonify({'code' : 1, 'token' : token})
 
 @auth.route('/logout')
 def logout():
